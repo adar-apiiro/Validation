4aa6c4fa3d8b40ef3c5b1b7cc7c117066bc7fe2d
adar-apiiro <144586914+adar-apiiro@users.noreply.github.com>
2024-05-28 16:53:19 +0300

    Create LoggingFramework.java

---
+++ Java/LoggingFramework.java
@@ -0,0 +1,21 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.pac4j.oauth.client.FacebookClient;
+import org.pac4j.oauth.client.Google2Client;
+
+public class ExampleUsage {
+    private static final Logger logger = LoggerFactory.getLogger(ExampleUsage.class);
+
+    public static void main(String[] args) {
+        FacebookClient facebookClient = new FacebookClient();
+        Google2Client google2Client = new Google2Client();
+
+        // Use the logger at the DEBUG level
+        logger.debug("FacebookClient and Google2Client have been initialized.");
+
+        performOAuthOperations(facebookClient, google2Client);
+    }
+    private static void performOAuthOperations(FacebookClient facebookClient, Google2Client google2Client) {
+        logger.debug("Performing OAuth operations...");
+    }
+}
