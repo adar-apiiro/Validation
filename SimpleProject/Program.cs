81be7fa4d3671371db166b09264e548a4f11b2b3
adar-apiiro <144586914+adar-apiiro@users.noreply.github.com>
2024-06-05 18:21:06 +0300

    Add files via upload

---
+++ SimpleProject/Program.cs
@@ -0,0 +1,42 @@
+
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using log4net;
+
+namespace SimpleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new MyDbContext())
+            {
+                var data = context.MyEntities.ToList();
+                foreach (var entity in data)
+                {
+                    Console.WriteLine($"Id: {entity.Id}, Name: {entity.Name}");
+                }
+            }
+
+            var log = LogManager.GetLogger(typeof(Program));
+            log.Info("Hello, log4net!");
+        }
+    }
+
+    public class MyDbContext : IdentityDbContext
+    {
+        public DbSet<MyEntity> MyEntities { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("your_connection_string");
+        }
+    }
+
+    public class MyEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ SimpleProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using log4net.Config;
+
+[assembly: AssemblyTitle("SimpleProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SimpleProject")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d25c5f17-7fc3-4e47-a746-2cf94663fa95")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: XmlConfigurator(ConfigFile = "log4net.config")]
